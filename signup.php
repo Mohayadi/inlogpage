7b55eff4d7602b011522ef9c3c1cf1e2262fa2a9
osama <129272439+OsamaElAnzi@users.noreply.github.com>
2023-09-06 16:38:22 +0200

    test nummer 2 connection

--- signup.php
+++ signup.php
@@ -5,19 +5,19 @@ include 'DBconnection.php';
 
     <section class="signup-form">
         <h2>Sign up</h2>
-        <form action="signup.inc.php" method="post">
-            <input type="text" name="name" placeholder="Name...">
+        <form action="" method="POST">
+            <input type="text" name="email" placeholder="Email...">
             <input type="password" name="pwd" placeholder="Password...">
-            <button type="submit" name="submit" value="">Sign up</button>
+            <button type="submit" name="submit">Sign up</button>
         </form>
     </section>
 <?php
 
 $pdo = 'SELECT pw, email FROM loginpage';
 
-$submit = $_POST['submit'];
+$submit = $_POST["submit"];
 if (isset($submit)) {
-    $Name = $_POST['name'];
+    $Name = $_POST["name"];
     $Password = $_POST['pwd'];
     echo 'succesfully';
 }
