59924e34f000c39f8114e8c64d11d4d56d368d38
Mohayadi <59573745+Mohayadi@users.noreply.github.com>
2023-09-06 15:44:05 +0200

    Update and rename index.php to signup.php

---
+++ signup.php
@@ -0,0 +1,12 @@
+<?php
+?>
+
+
+    <section class="signup-form">
+        <h2>Sign up</h2>
+        <form action="signup.inc.php" method="post">
+            <input type="text" name="name" placeholder="Name...">
+            <input type="password" name="pwd" placeholder="Password...">
+            <button type="submit" name="submit">Sign up</button>
+        </form>
+    </section>
